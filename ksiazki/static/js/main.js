e11bf2f6296cf4875b1e795901b9c9f8eb4aeda1
Dawid <dawidfirmowski@gmail.com>
2023-05-17 18:22:56 +0200

    css and js

--- ksiazki/static/js/main.js
+++ ksiazki/static/js/main.js
@@ -1,3 +1,21 @@
-body = document.querySelector('body')
-changeThemeButton = document.querySelector('#change-theme')
+const body = document.querySelector('body');
+const changeThemeButton = document.querySelector('#theme-toogle');
+const themeKey = 'preferred-theme';
 
+const preferredTheme = localStorage.getItem(themeKey);
+
+if (preferredTheme) {
+    body.classList.add(preferredTheme);
+}
+
+changeThemeButton.addEventListener('click', () => {
+    if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        localStorage.setItem(themeKey, 'light-theme')
+    } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        localStorage.setItem(themeKey, 'dark-theme')
+    }
+});
